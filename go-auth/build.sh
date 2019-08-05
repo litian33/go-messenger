e2f275283e8ab1bd00b35073eda371c161083040
litian <litian33@gmail.com>
2019-08-05 16:48:10 +0800

    modify test

--- go-auth/build.sh
+++ go-auth/build.sh
@@ -3,6 +3,6 @@ set -e
 docker run --rm -it \
     -v $PWD:/go/src/go-messenger/go-auth/ \
     -e SOURCE_PATH=go-messenger/go-auth/ \
-    usman/go-builder:1.8
+    litian/go-builder:1.12
 
-docker build -t usman/go-auth .
+docker build -t litian/go-auth .
